003fe27c5ba52e4d0fd7119cd1432c97a8c0d808
badapureprogrammer <bcomprogrammeryahoo@gmail.com>
2025-03-18 13:14:28 +0530

    Add - Navabr Structure with Functionality

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("website loaded")
+let toggle = document.getElementById("toggle");
+let list = document.getElementById("nav-list");
+
+toggle.addEventListener("click",(e)=>{
+    let isTrusted = e.isTrusted
+
+    if(isTrusted){
+        if(e.srcElement.className == "fa-solid fa-bars"){
+            e.srcElement.className = "fa-solid fa-xmark";
+            list.className = "list-item"
+
+        }else if(e.srcElement.className == "fa-solid fa-xmark"){
+            e.srcElement.className = "fa-solid fa-bars"
+            list.className = "media-navbar";
+        }
+    }
+})
+// End of Navbar Functionality
